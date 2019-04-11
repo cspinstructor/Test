4ef7582e56f9b1d9ef6d7e53f09a962170f416fc
paulchin <paulchin20@yahoo.com>
2019-04-11 21:33:38 +0800

    first commit

---
+++ platforms/browser/www/app.js
@@ -0,0 +1,11 @@
+function showBannerAd() {
+  alert('show Banner Ad');
+}
+
+function showInterstitialAd() {
+  alert('show Interstitial Ad');
+}
+
+function showRewardedVideoAd() {
+  alert('show Rewarded Video Ad');
+}
